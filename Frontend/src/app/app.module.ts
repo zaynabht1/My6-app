4ee423cb369064fc7d158a53fe44201373ba695b
zaynabht1 <zaynab.ht@gmail.com>
2021-01-19 14:58:30 +0200

    routing 1

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,23 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import {HttpClientModule} from '@angular/common/http';
+import {Routes, RouterModule} from '@angular/router';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PropertyListComponent } from './property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { PropertyCardComponent } from './property-card/property-card.component';
+import { AddPropertyComponent } from './add-property/add-property.component';
+import { PropertyDetailComponent } from './property-detail/property-detail.component';
+
+
+const appRoutes: Routes = [
+  {path: 'add-property', component: AddPropertyComponent},
+    // add -property here is the name of the path only not the name of the component
+  {path: '', component: PropertyListComponent},
+  {path: 'rent-property', component: PropertyListComponent},
+  {path: 'property-detail/:id', component: PropertyDetailComponent}
+
+
+
+
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     PropertyListComponent,
     NavBarComponent,
-    PropertyCardComponent
-  ],
+    PropertyCardComponent,
+    AddPropertyComponent,
+    PropertyDetailComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
