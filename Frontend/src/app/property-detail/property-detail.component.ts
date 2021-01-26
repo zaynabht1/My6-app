7ce1ae9adf5dec38d46b9db63675d0692b818354
zaynabht1 <zaynab.ht@gmail.com>
2021-01-26 13:22:47 +0200

    routing 3

--- Frontend/src/app/property-detail/property-detail.component.ts
+++ Frontend/src/app/property-detail/property-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { stringify } from  'querystring';
 
 @Component({
   selector: 'app-property-detail',
@@ -7,12 +8,19 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./property-detail.component.css']
 })
 export class PropertyDetailComponent implements OnInit {
-public propertyId: number;
 
-  constructor(private route: ActivatedRoute) { }
+ public propertyId: number=0;
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-    this.propertyId= this.route.snapshot.params['id'];
+    this.propertyId= Number(this.route.snapshot.params['id']);
+  }
+
+  onSlelectNext(){
+    this.propertyId+=1;
+    this.router.navigate(['property-detail', this.propertyId ]);
+
   }
 
 }
